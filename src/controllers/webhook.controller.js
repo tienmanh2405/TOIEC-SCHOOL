cfaf957a0ccf4544a7c6e72a9ed3b581be66106c
tienmanh2405 <tienmanh0167@gmail.com>
2024-07-28 19:48:06 +0700

    test

--- src/controllers/webhook.controller.js
+++ src/controllers/webhook.controller.js
@@ -1,12 +1,19 @@
 import DangKyHoc from '../models/dangkyhoc.model.js';
-
+import db from '../connectData/connectSql.js';
 const handleStripeWebhook = async (req, res) => {
     try {
         const { registrationId } = req.body;
         const MaDangKy = registrationId;  
         const updateDangKy = await DangKyHoc.update(
             { TrangThaiThanhToan: true, clientSecret: null }, MaDangKy);
-        
+        const sql = 'CALL taoLopHocVaThemHocVien()';
+        db.query(sql, (err, results) => {
+          if (err) {
+            res.status(500).send(err);
+          } else {
+            res.status(200).json(results[0]);
+          }
+        });
         res.status(200).json({ received: true, success: true, update: updateDangKy });
     } catch (error) {
         console.error('Error updating registration:', error);
